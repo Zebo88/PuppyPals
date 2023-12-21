a9c61d18e84e2d1a53db814fe90bc79618c00d62
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2023-12-21 11:02:24 -0700

    Created puppyList and imported to App. Rendered puppyList to webpage and created functionality for click event that shows puppy details.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import './App.css'
+import { useState } from 'react';
+import { puppyList } from './data.js';
+import './App.css';
 
 function App() {
 
-  return (
-    <div>
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick() {
+
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  return (
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return (
+            <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })
+      }
+      { 
+        featPupId &&
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      }
     </div>
   )
 }

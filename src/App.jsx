37d644704a1dff31c51dbe5644ef8249ffc4aa1d
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2023-12-21 13:11:42 -0700

    Added CSS: color-scheme, font style, images folder with paw print jpg, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
 import { useState } from 'react';
 import { puppyList } from './data.js';
 import './App.css';
+import pawprint from './images/paw-solid.svg';
 
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick() {
+  // function handleClick() {
 
-  }
+  // }
 
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup);
 
   return (
     <div className='App'>
-      {
-        puppies.map((puppy) => {
+      { 
+        <div className='puppy-name-container'>
+        {puppies.map((puppy) => {
           return (
-            <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+            <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id} className='puppy-name'>
               {puppy.name}
             </p>
           );
         })
+        }
+        </div>
       }
       { 
         featPupId &&
-        <div>
+        <div className='details-container'>
           <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
+          <img src={pawprint} alt="paw print" />
         </div>
       }
     </div>
